01315744c7eaf725402056befe33cbe9bed4834e
Aishwarya Kulshrestha <77093969+Aishwarya-Kulshrestha@users.noreply.github.com>
2021-01-25 04:05:51 +0530

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h>     //header for malloc() and exit()
 
 int visited[5]={0,0,0,0,0};
-int flag=0;
+int flag=0;        //flag is set to 1 when the node is found
 
 struct node
 {
@@ -10,8 +10,8 @@ struct node
 	struct node *link;
 };
 
-struct node *rear,*front, *Q;     
- 
+struct node *rear,*front, *Q;     /*Q just stores the start of Linked list.. front does that too, but 
+					i will use Q as prober later in the code*/
 int enqueue(int i)  
 {
 	
@@ -51,18 +51,18 @@ int dequeue()
 
 int bfs(int G[5][5],int v,int d)
 {
-	visited[v]=1;
-	if(v==d) 
+	visited[v]=1;           //sets the position of the node to 1 indicating that it has been visited
+	if(v==d) 	//check if the dest. node is found, it returns 1
 	{ 
 		printf("%d",v);
 		return 1;
 	}
 	printf("%d ->",v);
-	for(int i=0;i<5;i++)
+	for(int i=0;i<5;i++)    //loads the corresponding nodes in the queue
 		if(G[v][i]==1 && visited[i]==0)
 			enqueue(i);
 	
-	while(Q)
+	while(Q)    //loops until all nodes are dequeued and the queue is empty
 	{
 		if(visited[Q->data]==0 && flag==0) 
 			flag=bfs(G,dequeue(),d);
@@ -83,14 +83,14 @@ int main()
 		     {0,0,1,0,1}};
 	printf("\nEnter Start node: ");
 	scanf("%d",&v);
-	if(v<0 || v>4) 
+	if(v<0 || v>4)         //check if entry out of bound
 	{
 		printf("\nInvalid entry\n");
 		exit(0);
 	}
 	printf("\nEnter End node: ");
 	scanf("%d",&d);
-	if(v<0 || v>4) 
+	if(v<0 || v>4) 		//check if entry out of bound
 	{
 		printf("\nInvalid entry\n");
 		exit(0);
